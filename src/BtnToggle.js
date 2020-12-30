ddf3d29b8efaad61c240b59d4f58850146d4666f
Nicolas Hervy <nicolas.hervy@spark-vision.com>
2020-12-31 00:04:06 +0100

    Improved and simplified zoom UX.

--- src/BtnToggle.js
+++ src/BtnToggle.js
@@ -1,70 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const ZoomToggle = styled.div`
-  --border-style: 1px solid white;
-  --width: 2em;
-  --height: calc(var(--width) / 2);
-  --frame-padding: calc(var(--height) / 6);
-  --knob-size: calc(var(--height) - var(--frame-padding) * 2);
+  --size: 7em;
   position: absolute;
-  left: 0;
-  bottom: 0;
-  display: inline-flex;
-  align-items: center;
-  justify-content: flex-start;
-  div.frame {
-    position: relative;
-    display: flex;
-    align-items: center;
-    margin: 0.5em;
-    width: var(--width);
-    height: var(--height);
-    border-radius: calc(var(--height) / 2);
-    border: var(--border-style);
-    padding: 1px;
-    cursor: pointer;
-    background-color: rgba(255, 255, 255, 0);
-    &.active {
-      background-color: rgba(255, 255, 255, 0.5);
-      div.knob {
-        left: calc(var(--width) - var(--height));
-        background-color: rgba(255, 255, 255, 0.9);
-      }
+  left: calc(var(--size) / -2);
+  bottom: calc(var(--size) / -2);
+  background-color: black;
+  width: var(--size);
+  height: var(--size);
+  border-radius: 50%;
+  transition: transform 500ms ease-in-out;
+  transform: rotate(0deg);
+  transform-origin: center center;
+  cursor: pointer;
+  &.active {
+    transform: rotate(-90deg);
+  }
+  &:hover i {
+    &.fa-search-minus {
+      transform: translate(-50%, -50%) scale(1.5);
     }
-    div.knob {
-      position: relative;
-      left: 0;
-      width: var(--knob-size);
-      height: var(--knob-size);
-      transition: left 0.4s linear;
-      border: var(--border-style);
-      border-radius: calc(var(--knob-size) / 2);
+    &.fa-search-plus {
+      transform: translate(-50%, -50%) scale(1.5) rotate(90deg);
     }
   }
-  label {
-    opacity: 0.7;
-    white-space: nowrap;
-    color: white;
-    text-transform: uppercase;
-    font-size: 0.7em;
-    i {
-      font-size: 1.5em;
+  i {
+    color: rgba(255, 255, 255, 1);
+    position: absolute;
+    transition: transform 200ms ease-out;
+    &.fa-search-minus {
+      left: 70%;
+      top: 30%;
+      transform: translate(-50%, -50%);
+    }
+    &.fa-search-plus {
+      left: 70%;
+      top: 70%;
+      transform: translate(-50%, -50%) rotate(90deg);
     }
   }
 `;
 ZoomToggle.displayName = 'BtnToggle';
 
 export default function BtnToggle() {
+  const [active, setActive] = useState(false);
+
+  function toggle() {
+    setActive(!active);
+  }
+
   return (
-    <ZoomToggle>
-      <div className="frame">
-        <div className="knob"></div>
-      </div>
-      <label>
-        {/* <i className="fas fa-search-plus"></i> */}
-        zoom out
-      </label>
+    <ZoomToggle
+      className={`zoom-toggle ${active ? 'active' : ''}`}
+      onClick={() => toggle()}
+    >
+      <i className="fas fa-search-minus"></i>
+      <i className="fas fa-search-plus"></i>
     </ZoomToggle>
   );
 }
