e31b0944b9a891be45a4e2fd4c4c010e0715a646
Nicolas Hervy <nicolas.hervy@spark-vision.com>
2021-01-11 15:15:28 +0100

    clx added. Use gesture added. side swipe WIP.

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -5,9 +5,12 @@ import Portal from './Portal';
 import motionBlur from './utils/motion-blur-move';
 import microState from './utils/microState';
 import preloadImages from './utils/preloadImages';
-import _ from 'lodash';
+// import _ from 'lodash';
+import clsx from 'clsx';
+import { usePinch } from 'react-use-gesture';
 
 /* TODO
+- Test the 'touchmove' event for iphone.
 - Make zoom to a boolean setting.
 - Make show unselected images a boolean setting.
 - Create overscroll animation on slide endpoints. DONE
@@ -114,7 +117,7 @@ const slideCards = ({ nextIndex }) => {
   }).then(({ element }) => {
     SET({ CARDS_MOVING: 0 });
     _setSelectedCardIndex(index);
-    console.log('done', element);
+    // console.log('done', element);
   });
   function _setState({ index }) {
     let CARDS_MOVING = 0;
@@ -136,6 +139,7 @@ const stateGuiMediator = () => {
   const firstImage = cardsCollection[0].querySelector('img');
   const documentRoot = document.documentElement;
   const cardContainer = document.querySelector('.card-container');
+  const zoomBtn = document.querySelector('.zoom-toggle');
   // Micro state helper functions.
   const { ADD, SET, STATE, SET_COMPLEX, REMOVE } = microState();
   return {
@@ -143,6 +147,7 @@ const stateGuiMediator = () => {
     cardsWrapper,
     cardContainer,
     firstImage,
+    zoomBtn,
     documentRoot,
     ADD,
     SET,
@@ -226,7 +231,7 @@ const handleGestures = (e, setZoomedOut) => {
     }
     async function overScrollAnim(dir) {
       if (debounce()) return;
-      const overScrollDist = -20;
+      const overScrollDist = -40;
       // eslint-disable-next-line
       const regexParentesisContent = /\(([^\)]*)\)/;
       const cardsWrapperStyles = window.getComputedStyle(cardsWrapper);
@@ -239,7 +244,7 @@ const handleGestures = (e, setZoomedOut) => {
             )
           : 0;
       await motionBlur(cardsWrapper, {
-        durationMs: 150,
+        durationMs: 100,
         properties: [
           {
             property: 'transform',
@@ -250,7 +255,7 @@ const handleGestures = (e, setZoomedOut) => {
         easing: 'easeOutQuad',
       });
       motionBlur(cardsWrapper, {
-        durationMs: 350,
+        durationMs: 250,
         properties: [
           {
             property: 'transform',
@@ -272,17 +277,29 @@ const handleGestures = (e, setZoomedOut) => {
   }
 };
 
+const doOnPinch = (state, setZoomedOut) => {
+  if (state.pinching) {
+    setZoomedOut(state.vdva[0] < 0 ? true : false);
+  }
+};
+
+// Prevent accesability zoom in safari/iphone.
 document.addEventListener('gesturestart', (e) => e.preventDefault());
 document.addEventListener('gesturechange', (e) => e.preventDefault());
 
 function Slider() {
   console.log('rendered');
+  const domTarget = useRef(null);
   const [options, setOptions] = useState([]);
   const [preloading, setPreloading] = useState(true);
   const [hideSlider, setHideSlider] = useState(false);
   const cardContainer = useRef();
   const [zoomedOut, setZoomedOut] = useState(false);
   const [, setScrollDisabled] = usePreventScroll(true);
+  usePinch((state) => doOnPinch(state, setZoomedOut), {
+    domTarget,
+    eventOptions: { passive: false },
+  });
 
   const toggleZoomInOut = () => setZoomedOut(!zoomedOut);
 
@@ -303,75 +320,117 @@ function Slider() {
       setOptions(cardsArr);
       await initiate();
       setHideSlider(false);
-      setScrollDisabled(true);
+      if (true) setScrollDisabled(true);
       console.log('React inited');
     };
     init();
   }, [setOptions, setScrollDisabled]);
 
   useEffect(() => {
-    const el = cardContainer.current.parentElement;
-    el.addEventListener(
-      'wheel',
-      _.throttle((e) => handleGestures(e, setZoomedOut), 800, {
-        leading: true,
-        trailing: false,
-      })
-    );
+    // const el = cardContainer.current.parentElement;
+    // el.addEventListener(
+    //   'wheel',
+    //   _.throttle((e) => handleGestures(e, setZoomedOut), 250, {
+    //     leading: true,
+    //     trailing: false,
+    //   })
+    // );
+    // el.addEventListener(
+    //   'touchdown',
+    //   _.throttle((e) => console.log('touch down'), 250, {
+    //     leading: true,
+    //     trailing: false,
+    //   })
+    // );
+    // el.addEventListener(
+    //   'touchmove',
+    //   _.throttle(
+    //     (e) => {
+    //       // e = e.originalEvent || e;
+    //       console.log('e.deltaX', e);
+    //       // alert('touched deltaX: ' + e.deltaX);
+    //       const event = new WheelEvent('wheel', {
+    //         bubbles: true,
+    //         cancelable: true,
+    //         view: window,
+    //       });
+    //       window.dispatchEvent(event);
+    //       // const event = document.createEvent('HTMLEvents');
+    //       // event.initEvent('wheel', true, true);
+    //       // event.eventName = 'wheel';
+    //       // window.dispatchEvent(event);
+    //       // handleGestures(e, setZoomedOut);
+    //     },
+    //     800,
+    //     {
+    //       leading: true,
+    //       trailing: false,
+    //     }
+    //   )
+    // );
     window.addEventListener('click', clickActions);
+    const clickInsideZoomToggleBtn = (e) => !!e.target.closest('.zoom-toggle');
+    //
     async function clickActions(e) {
+      if (clickInsideZoomToggleBtn(e)) return;
       if (zoomedOut) setZoomedOut(false);
       await handleClick(e);
     }
     return () => {
-      el.removeEventListener(
-        'wheel',
-        _.throttle((e) => handleGestures(e, setZoomedOut), 800, {
-          leading: true,
-          trailing: false,
-        })
-      );
-      window.removeEventListener('click', clickActions);
+      // el.removeEventListener(
+      //   'wheel',
+      //   _.throttle((e) => handleGestures(e, setZoomedOut), 250, {
+      //     leading: true,
+      //     trailing: false,
+      //   })
+      // );
+      // window.removeEventListener('click', clickActions);
     };
   }, [zoomedOut]);
 
+  const cardContainerStyles = clsx({
+    'card-container': true,
+    ispreloading: preloading,
+    'zoomed-out': zoomedOut,
+  });
+
   return (
-    <section
-      ref={cardContainer}
-      className={`card-container${preloading ? ' ispreloading' : ''}${
-        zoomedOut ? ' zoomed-out' : ''
-      }`}
-    >
-      <div className={`card-wrapper${hideSlider ? ' transparent' : ''}`}>
-        {!preloading ? (
-          options.map((o, i) => (
-            <div key={i} className="card-section center" data-index={i}>
-              <div className="image">
-                <img src={o.imageUrl} alt={o.header} />
+    <div ref={domTarget} style={{ width: '100%', height: '100%' }}>
+      <section ref={cardContainer} className={cardContainerStyles}>
+        <div className={`card-wrapper${hideSlider ? ' transparent' : ''}`}>
+          {!preloading ? (
+            options.map((o, i) => (
+              <div key={i} className="card-section center" data-index={i}>
+                <div className="image">
+                  <img src={o.imageUrl} alt={o.header} />
+                </div>
+                <div className="header">
+                  <h4>{o.header}</h4>
+                </div>
+                <div className="paragraph">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Accusantium quidem nostrum veritatis odio, maiores quasi?
+                </div>
               </div>
-              <div className="header">
-                <h4>{o.header}</h4>
+            ))
+          ) : (
+            <div className="card-section center preloader-card">
+              <div className="image animated-background"></div>
+              <div className="header animated-background">
+                <h4>&nbsp;</h4>
               </div>
-              <div className="paragraph">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium quidem nostrum veritatis odio, maiores quasi?
-              </div>
+              <div className="paragraph animated-background">&nbsp;</div>
             </div>
-          ))
-        ) : (
-          <div className="card-section center preloader-card">
-            <div className="image animated-background"></div>
-            <div className="header animated-background">
-              <h4>&nbsp;</h4>
-            </div>
-            <div className="paragraph animated-background">&nbsp;</div>
-          </div>
-        )}
-      </div>
-      <Portal>
-        <BtnToggle toggleZoomInOut={toggleZoomInOut}></BtnToggle>
-      </Portal>
-    </section>
+          )}
+        </div>
+        <Portal>
+          <BtnToggle
+            toggleZoomInOut={toggleZoomInOut}
+            zoomedOut={zoomedOut}
+          ></BtnToggle>
+        </Portal>
+      </section>
+    </div>
   );
 }
 
